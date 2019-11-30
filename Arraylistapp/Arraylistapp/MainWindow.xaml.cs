3625c8456510ccd6bc9180a362979c746a1e7cf2
kittipon01 <58304014+kittipon01@users.noreply.github.com>
2019-11-30 20:35:18 +0700

    ArrayListApp coding

---
+++ Arraylistapp/Arraylistapp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arraylistapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayListName;
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayListName = new ArrayList();
+
+        }
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {   
+            //Show Data
+            foreach (string data in arrayListName)
+            {
+                MessageBox.Show("Name "+data);
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            arrayListName.Add(txtName.Text);
+
+        }
+
+        private void button_Remove_Click(object sender, RoutedEventArgs e)
+        {
+            arrayListName.Remove(txtName.Text);
+        }
+    }
+}
