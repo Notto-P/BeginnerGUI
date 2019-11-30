fcdc4b62196b392b50a1da31563eca3f9c9702b2
kittipon01 <58304014+kittipon01@users.noreply.github.com>
2019-11-30 21:23:49 +0700

    SortedList

---
+++ SortedListApp/SortedListApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortedListApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedlist;
+        string data;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedlist = new SortedList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            sortedlist.Add(int.Parse(txtkey.Text),txtvalue.Text);
+            ICollection collection = sortedlist.Keys;
+
+            foreach (int key in collection)
+            {
+                data = data +" Value : " +sortedlist[key].ToString();
+            }
+            MessageBox.Show(data);
+            data = "";
+        }
+    }
+}
