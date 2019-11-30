9316f865f006de0508a6c1413d7624484412b4bc
kittipon01 <58304014+kittipon01@users.noreply.github.com>
2019-11-30 20:59:29 +0700

    Hashmap

---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void btnshow_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            //Show
+            foreach (string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+
+        private void btnadd_Click(object sender, RoutedEventArgs e)
+        {
+            //ADD
+            hashtable.Add(txtkey.Text,txtvalue.Text);
+        }
+
+        private void btnre_Click(object sender, RoutedEventArgs e)
+        {
+            //Remove
+            hashtable.Remove(txtkey.Text);
+        }
+    }
+}
