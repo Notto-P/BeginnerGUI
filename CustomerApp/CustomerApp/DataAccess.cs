447c033428f0d718ef5e821d164fcda9a789958c
kittipon01 <58304014+kittipon01@users.noreply.github.com>
2019-12-01 15:45:17 +0700

    Sqlite

---
+++ CustomerApp/CustomerApp/DataAccess.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using Sitecore.FakeDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApp
+{
+    class DataAccess
+    {
+        public async static void InitializeDatabase()
+        {
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=SqlCus"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (uid INTEGER PRIMARY KEY, " +
+                    "first_Name VARCHAR(30) NULL, lirst_Name VARCHAR(40) NULL, email VARCHAR(200) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
+        public static void AddData(string inputText)
+        {
+            using (SqliteConnection db =
+                new SqliteConnection("Filename=SqlCus"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO Customers VALUES (4, @firstName, @lastName, @email);";
+                insertCommand.Parameters.AddWithValue("@firstName", inputText);
+                insertCommand.Parameters.AddWithValue("@lastName", inputText);
+                insertCommand.Parameters.AddWithValue("@email", inputText);
+
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+    }
+}
---
+++ CustomerApp/CustomerApp/Selectdata.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApp
+{
+    class Selectdata
+    {
+        
+        public static List<string> GetData()
+        {
+            List<string> entries = new List<string>();
+            //string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "SqlCus.db");
+            using (SqliteConnection db = new SqliteConnection($"Filename=SqlCus"))
+            {
+                db.Open();
+
+                SqliteCommand selectcommand = new SqliteCommand
+                    ("SELECT uid, first_Name, lirst_Name, email from Customers", db);
+                SqliteDataReader query = selectcommand.ExecuteReader();
+               
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                    entries.Add(query.GetString(3));
+                }
+
+                db.Close();
+            }
+
+            return entries;
+
+        }
+    }
+}
